60d2cb31dd3e4444b02893851a0f79c217af1eec
YhosuaVBarrios <yvidal@emergya.com>
2025-03-19 10:04:35 +0100

    prettier formato

--- src/requests/examples/applications/cats/request.ts
+++ src/requests/examples/applications/cats/request.ts
@@ -1,6 +1,6 @@
-import { request, spec } from 'pactum';  //Importa la configuracion de la peticion headers contraseña usuarios ...
-import { ObjectType } from '@classes/objectType'; //Configuracion para any ObjectType
-import LoadEnvironment from '@utils/loadEnvironment';  // Iporta  lacalseLoadEnviroment que carga las configuraciones del entorno
+import { request, spec } from "pactum"; //Importa la configuracion de la peticion headers contraseña usuarios ...
+import { ObjectType } from "@classes/objectType"; //Configuracion para any ObjectType
+import LoadEnvironment from "@utils/loadEnvironment"; // Iporta  lacalseLoadEnviroment que carga las configuraciones del entorno
 
 class Request {
   private static REQUEST: Request;
@@ -9,9 +9,11 @@ class Request {
 
   private RETRIES = 4;
 
-  private FIXTURES_PATH = 'examples/applications/cats';
+  private FIXTURES_PATH = "examples/applications/cats";
 
-  private RESOURCE = LoadEnvironment.getFixture(`${this.FIXTURES_PATH}/resource/pointCats.json`);
+  private RESOURCE = LoadEnvironment.getFixture(
+    `${this.FIXTURES_PATH}/resource/pointCats.json`
+  );
 
   static getInstance(): Request {
     if (!Request.REQUEST) {
@@ -19,41 +21,54 @@ class Request {
     }
     return Request.REQUEST;
   }
-  
+
   /** catsRequests
    * @description Get the dragon-ball characters list
    */
-  async getCats(): Promise<ObjectType> {           //Retornamos un object
-    request.setDefaultTimeout(this.TIMEOUT);            //serdefaultTimeout es propio de pactum y pasamos variable con 20000
-    return spec()                                         //Crea una nueva prueba en pactum
-      .get(`${LoadEnvironment.getInfo('urlBase')}${this.RESOURCE.get.cats.path}`)  //Peticion get //cambiar.path /fact
+  async getCats(): Promise<ObjectType> {
+    //Retornamos un object
+    request.setDefaultTimeout(this.TIMEOUT); //serdefaultTimeout es propio de pactum y pasamos variable con 20000
+    return spec() //Crea una nueva prueba en pactum
+      .get(
+        `${LoadEnvironment.getInfo("urlBase")}${this.RESOURCE.get.cats.path}`
+      ) //Peticion get //cambiar.path /fact
       .withHeaders(this.RESOURCE.get.cats.headers)
       .expectStatus(200)
-      .retry(this.RETRIES, this.TIMEOUT - 1);  //delay
+      .retry(this.RETRIES, this.TIMEOUT - 1); //delay
   }
 
   /** catsRequests
    * @description Get the dragon-ball characters list
    */
-  async getCatsList(): Promise<ObjectType> {           //Retornamos un object
-    request.setDefaultTimeout(this.TIMEOUT);            //serdefaultTimeout es propio de pactum y pasamos variable con 20000
-    return spec()                                         //Crea una nueva prueba en pactum
-      .get(`${LoadEnvironment.getInfo('urlBase')}${this.RESOURCE.get.catsList.path}`)  //Peticion get //cambiar.path /fact
+  async getCatsList(): Promise<ObjectType> {
+    //Retornamos un object
+    request.setDefaultTimeout(this.TIMEOUT); //serdefaultTimeout es propio de pactum y pasamos variable con 20000
+    return spec() //Crea una nueva prueba en pactum
+      .get(
+        `${LoadEnvironment.getInfo("urlBase")}${
+          this.RESOURCE.get.catsList.path
+        }`
+      ) //Peticion get //cambiar.path /fact
       .withHeaders(this.RESOURCE.get.catsList.headers)
       .expectStatus(200)
-      .retry(this.RETRIES, this.TIMEOUT - 1);  //delay
+      .retry(this.RETRIES, this.TIMEOUT - 1); //delay
   }
 
-   /** catsRequests
+  /** catsRequests
    * @description Get the dragon-ball characters list
    */
-   async getCatsBreeds(): Promise<ObjectType> {           //Retornamos un object
-    request.setDefaultTimeout(this.TIMEOUT);            //serdefaultTimeout es propio de pactum y pasamos variable con 20000
-    return spec()                                         //Crea una nueva prueba en pactum
-      .get(`${LoadEnvironment.getInfo('urlBase')}${this.RESOURCE.get.catsBreeds.path}`)  //Peticion get //cambiar.path /fact
+  async getCatsBreeds(): Promise<ObjectType> {
+    //Retornamos un object
+    request.setDefaultTimeout(this.TIMEOUT); //serdefaultTimeout es propio de pactum y pasamos variable con 20000
+    return spec() //Crea una nueva prueba en pactum
+      .get(
+        `${LoadEnvironment.getInfo("urlBase")}${
+          this.RESOURCE.get.catsBreeds.path
+        }`
+      ) //Peticion get //cambiar.path /fact
       .withHeaders(this.RESOURCE.get.catsBreeds.headers)
       .expectStatus(200)
-      .retry(this.RETRIES, this.TIMEOUT - 1);  //delay
+      .retry(this.RETRIES, this.TIMEOUT - 1); //delay
   }
 }
 export default Request;
--- src/utils/loadEnvironment.ts
+++ src/utils/loadEnvironment.ts
@@ -1,27 +1,34 @@
-import { ObjectType } from '@classes/objectType'; //Importa la clase para usar ObectType
-import minimist from 'minimist'; //Biblioteca que analiza comandos CLI
-import fs from 'fs'; //Permite leer archivos Json conconfiguraciones y datos de prueba
-import path from 'path'; //Facilita la construcionde rutas de archivos compatible conn todos los sistemas op
+import { ObjectType } from "@classes/objectType"; //Importa la clase para usar ObectType
+import minimist from "minimist"; //Biblioteca que analiza comandos CLI
+import fs from "fs"; //Permite leer archivos Json conconfiguraciones y datos de prueba
+import path from "path"; //Facilita la construcionde rutas de archivos compatible conn todos los sistemas op
 
 class LoadEnvironment {
-  private static info: ObjectType;  //almacena informacion sobre el entorno configuracions
+  private static info: ObjectType; //almacena informacion sobre el entorno configuracions
 
   private static environment: string; //Guarda el nombre del entrno dev,stangin...
 
-  static getInfo(varName: string): ObjectType {                     //Devuelve el valor de una variable de entorno desde un archivo jso// En string recoge la variable de consola
+  static getInfo(varName: string): ObjectType {
+    //Devuelve el valor de una variable de entorno desde un archivo jso// En string recoge la variable de consola
     return LoadEnvironment.getEnvironmentInfo()[varName]; //Se asegura llamando  a getEnviromentInfo para ver si esta cargado los datos de entorno con nuestra variable
   }
 
-  private static getEnvironmentInfo(): LoadEnvironment {           //Cargar configuracion de entorno  si no esta cargado leel el Json del entorno
-    if (!LoadEnvironment.info) {                                   // Verifica si LoadEnviroment.info ya esta cargado
-      const environmentPath = path.resolve(__dirname, `../environments/${this.getEnvironment()}.json`);//reemplaza el valor de la variable enviroment que pasas por consola
-      const rawData = fs.readFileSync(environmentPath, 'utf-8');   // Lee el archivo json utilizando fs.readFileSync()
+  private static getEnvironmentInfo(): LoadEnvironment {
+    //Cargar configuracion de entorno  si no esta cargado leel el Json del entorno
+    if (!LoadEnvironment.info) {
+      // Verifica si LoadEnviroment.info ya esta cargado
+      const environmentPath = path.resolve(
+        __dirname,
+        `../environments/${this.getEnvironment()}.json`
+      ); //reemplaza el valor de la variable enviroment que pasas por consola
+      const rawData = fs.readFileSync(environmentPath, "utf-8"); // Lee el archivo json utilizando fs.readFileSync()
       LoadEnvironment.info = JSON.parse(rawData); //Pasa e json a un objeto Javascript utiliza el .parse
     }
-    return LoadEnvironment.info;   //Guarda los datos em Loadenviroment.info para leerlo en futuras llamadas Carga el fichero dondese buscaran los datos
+    return LoadEnvironment.info; //Guarda los datos em Loadenviroment.info para leerlo en futuras llamadas Carga el fichero dondese buscaran los datos
   }
 
-  static getArguments(): ObjectType {  //usa minimist para leer argumentos pasados al ejecutar el script
+  static getArguments(): ObjectType {
+    //usa minimist para leer argumentos pasados al ejecutar el script
     return minimist(process.argv);
   }
 
@@ -33,15 +40,21 @@ class LoadEnvironment {
    * E.g.: If the file is 'fixtures/ES/AV/quickReplies.json, the local path would be 'AV/quickReplies.json'.
    * @replacers Strings that are going to be included in the array strings.
    */
-  static getFixture(localPath: string): ObjectType { //carga archivos de prueba
+  static getFixture(localPath: string): ObjectType {
+    //carga archivos de prueba
     const { language } = this.getArguments();
     if (!language) {
-      console.error('Language has not been set. Please, indicate the language in the command as a parameter. '  //Si no tiene idioma para la ejecucion y muestra un error
-        + 'See README examples for more info.');
+      console.error(
+        "Language has not been set. Please, indicate the language in the command as a parameter. " + //Si no tiene idioma para la ejecucion y muestra un error
+          "See README examples for more info."
+      );
       process.exit(1);
     }
-    const filePath = path.resolve(__dirname, `../fixtures/${language}/${localPath}`); //Construye la ruta usando el idioma
-    const rawData = fs.readFileSync(filePath, 'utf-8');
+    const filePath = path.resolve(
+      __dirname,
+      `../fixtures/${language}/${localPath}`
+    ); //Construye la ruta usando el idioma
+    const rawData = fs.readFileSync(filePath, "utf-8");
 
     return JSON.parse(rawData);
   }
